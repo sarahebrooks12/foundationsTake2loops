543bc7754c119b79ed0a9c68214c202e34e910c1
sarahebrooks12 <sarahebrooks12@gmail.com>
2020-04-18 22:31:40 -0400

    did until brain melted

---
+++ script.js
@@ -0,0 +1,102 @@
+// In your JavaScript file, write a while loop that outputs the phrase "TWEET TWEET" 100 times to the console.
+
+var i = 0;
+
+while (i < 100) {
+    console.log("TWEET TWEET MOTHERFUCKERS");
+    i++
+}
+
+for (var i = 0; i < 100; i++) {
+    console.log("TWEET TWEET MOTHERFUCKERS")
+}
+
+// for (var i = 0; i < 100; i++) {
+    // console.log("TWEET TWEET TWEET TWEET TWEET")
+// }
+// Favorite Foods
+// In your JavaScript, create a new variable called favoriteFoods. It should store an array of strings representing five of your favorite foods.
+// Loop through the array and print each food item to the console.
+
+// var favoriteFoods = ["oreos", "cucumbers", "cereal", "chicken and dumplings", "peanut butter"]
+
+// for (var i = 0; i < favoriteFoods.length; i++) {
+    // console.log(favoriteFoods[i])
+// }
+
+// Student Names
+// In your JavaScript file, copy and paste the following array:
+// var cohortOneStudents = ["Sable", "David", "Joey", "Nick", "Tommy", "Connor", "Charles", "Russ", "Bobby", "Josh", "Nikki", "Matt", "Dale", "Sydney"]
+// Use a for loop to print each student's name to the console.
+
+// for (var i = 0; i < cohortOneStudents.length; i++) {
+//     console.log(cohortOneStudents[i])
+// }
+
+// 2. Interests
+// In your JavaScript file, create an array of 5 of your interests.
+// Loop through the interests and console log the phrase "One of my interests is: [your interest]." for each of the interests in your array.
+
+// var favoritePastTimes = ["reading", "online window shopping", "being outside", "socializing with friends", "spending time with family"]
+// for (var i = 0; i < favoritePastTimes.length; i++) {
+//     console.log(`One of my favorite things to do is ${favoritePastTimes[i]}.`)
+// }
+// Refactor your loop from the previous exercise. Pick one of your interests as your absolute favorite interest. Add a conditional to your loop to check if each interest in the array is your favorite. When the loop gets to your favorite interest, it should print "My absolute favorite interest is:" followed by the name of your interest. Any other interests can be printed the same way that they were in the previous exercise.
+
+// for (var i = 0; i <favoritePastTimes.length; i++){
+//     if (favoritePastTimes[i] === "reading") {
+//     console.log(`My absolute favorite thing is ${favoritePastTimes[0]}`)
+//     } else { 
+//         console.log(`One of my favorite things to do is ${favoritePastTimes[i]}.`)
+
+//     }
+// }
+// var harryPotterTitles = ["and the Sorcerer's Stone", "and the Chamber of Secrets", "and the Prisoner of Azkaban", "and the Goblet of Fire", "and the Order of the Phoenix", "and the Half-Blood Prince", "and the Deathly Hallows"];
+
+// Use a for loop to output the complete titles to the console.
+
+// for (i = 0; i < harryPotterTitles.length; i++) {
+//     console.log(`Harry Potter ${harryPotterTitles[i]}`)
+// }
+
+// Copy and paste the folowing array of grades into your JavaScript file:
+var grades = [92, 91, 75, 66, 52, 90, 83, 85, 64, 90, 72, 88, 77, 98, 100, 73, 92]
+// Loop through the grades with a for loop
+// If the current grade is greater than or equal to 0 and less than or equal to 69, log "You got an F" to the console.
+// If it's between 70 and 76, log "You got a D" to the console.
+// If it's between 77 and 84, log "You got a C" to the console.
+// If it's between 84 and 92, log "You got a B" to the console.
+// If it's between 93 and 100, log "You got an A" to the console.
+
+// for (i = 0; i < grades.length; i++) {
+//     if (grades[i] <= 76) {
+//         console.log("D")
+//     } else if (grades[i] <= 84) {
+//         console.log("C")
+//     } else if (grades[i] <= 92) {
+//         console.log("B")
+//     } else if (grades[i] <= 100) {
+//         console.log("A")
+//     }
+// }
+
+// Write a loop that counts by 2's and creates the following output.
+// Output
+// 2 4 6 8 Who do we appreciate?!
+// WHY WON'T YOU DO WHAT I WANT
+// for (i = 2; i <= 8; i++) 
+
+// console.log(" who do we appreciate?")
+
+
+
+var sentenceArray = ["the", "cow", "danced", "through", "the", "trees", "in", "the", "light", "of", "the", "moon"];
+console.log(sentenceArray)
+// Write a loop that builds sentenceArray into a real sentence, except every fourth word should be "MOOOOOO". Hint: look up the modulus operator.
+// Print your sentence ot the console.
+
+for (i = 0; i < sentenceArray.length; i++) {
+   
+}
+
+// I have no idea - tommy and jordan both showed me how. can't remember 
